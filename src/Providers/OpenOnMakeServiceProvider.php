fa1d2a8e9f5227847ff489a67442a72ca9d536ce
Andrew Huggins <andrewhuggins@gmail.com>
2018-04-13 16:50:53 -0400

    adjustment for listener to be triggered

--- src/Providers/OpenOnMakeServiceProvider.php
+++ src/Providers/OpenOnMakeServiceProvider.php
@@ -3,7 +3,6 @@
 namespace OpenOnMake\Providers;
 
 use Illuminate\Support\Facades\Event;
-use OpenOnMake\Listeners\OpenOnMake;
 use Illuminate\Support\ServiceProvider;
 
 class OpenOnMakeServiceProvider extends ServiceProvider
@@ -14,9 +13,9 @@ class OpenOnMakeServiceProvider extends ServiceProvider
             __DIR__.'/../config/open-on-make.php' => config_path('open-on-make.php')
         ], 'open-on-make');
 
-        Event::listen('Illuminate\Console\Events\CommandFinished', function($event)
-        {
-            new OpenOnMake($event);
-        });
+        Event::listen(
+            'Illuminate\Console\Events\CommandFinished',
+            'OpenOnMake\Listeners\OpenOnMake'
+        );
     }
 }
