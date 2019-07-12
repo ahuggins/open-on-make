a6324f18b15bdfaf4f8bb2bedd4579591f32cc14
Andrew Huggins <andrewhuggins@gmail.com>
2019-07-12 13:28:43 -0400

    Update open-on-make.php

--- src/config/open-on-make.php
+++ src/config/open-on-make.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'editor' => env('OPEN_ON_MAKE_EDITOR', 'subl'),
+    'editor' => env('OPEN_ON_MAKE_EDITOR', 'code'),
     'flags' => env('OPEN_ON_MAKE_FLAGS', ''),
     'enabled' => env('OPEN_ON_MAKE_ENABLED', true),
     /**
