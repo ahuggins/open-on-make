6287f6dfe99ce9b38de6e345d778b4a818b3135a
Andrew huggins <andrewhuggins@gmail.com>
2020-01-25 13:07:50 -0500

    move away from static methods in File for easier testing/mocking

--- src/File.php
+++ src/File.php
@@ -8,7 +8,7 @@ use OpenOnMake\Files\MigrationFile;
 
 class File
 {
-    public static function open($path)
+    public function open($path)
     {
         exec(
             config('open-on-make.editor') . ' ' .
@@ -17,24 +17,24 @@ class File
         );
     }
 
-    public static function openLatestMigration()
+    public function openLatestMigration()
     {
-        self::open(MigrationFile::getLatestMigrationFile());
+        $this->open(MigrationFile::getLatestMigrationFile());
     }
 
-    public static function getViewFileName($name)
+    public function getViewFileName($name)
     {
         $pathSeparator = str_replace('.', '/', $name) . '.blade.php';
         $parts = explode('/', $pathSeparator);
         return array_pop($parts);
     }
 
-    public static function getFileName($name)
+    public function getFileName($name)
     {
         return str_replace('\\', '/', $name . '.php');
     }
 
-    public static function filename($commandString, $name)
+    public function filename($commandString, $name)
     {
         if (Check::isViewCommand($commandString)) {
             return self::getViewFileName($name);
@@ -43,7 +43,7 @@ class File
         return self::getFileName($name);
     }
 
-    public static function find($filename)
+    public function find($filename)
     {
         $finder = new \Symfony\Component\Finder\Finder();
         $finder->files()->name($filename)->in(base_path());
@@ -60,29 +60,29 @@ class File
         return $path;
     }
 
-    public static function openAdditionalFile($path, $name, $option)
+    public function openAdditionalFile($path, $name, $option)
     {
-        File::open($path . $name . ucfirst(Options::getOption($option)) . '.php');
+        $this->open($path . $name . ucfirst(Options::getOption($option)) . '.php');
     }
 
-    public static function openAllTypes($name)
+    public function openAllTypes($name)
     {
         foreach (Options::getOptions() as $key => $value) {
             if (! Options::isMigration($value) && ! Options::isResource($value)) {
-                File::openAdditionalFile(Paths::getPath($value), $name, $key);
+                $this->openAdditionalFile(Paths::getPath($value), $name, $key);
             } elseif (Options::isMigration($value)) {
-                File::openLatestMigration();
+                $this->openLatestMigration();
             }
         }
     }
 
-    public static function openFilesGeneratedInAdditionToModel($option, $name)
+    public function openFilesGeneratedInAdditionToModel($option, $name)
     {
         if (Options::isMigration($option)) {
-            File::openLatestMigration();
+            $this->openLatestMigration();
         } else {
             $option = Options::isResource($option) ? '-c' : $option;
-            File::openAdditionalFile(Paths::getPath(Options::getOption($option)), $name, $option);
+            $this->openAdditionalFile(Paths::getPath(Options::getOption($option)), $name, $option);
         }
     }
 }
