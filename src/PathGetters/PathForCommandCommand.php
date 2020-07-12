719ee2a5f5bf6926f63a7f6670605251186588e9
Andrew huggins <andrewhuggins@gmail.com>
2020-07-12 02:35:33 -0400

    specific code for how to open can be isolated

---
+++ src/PathGetters/PathForCommandCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OpenOnMake\PathGetters;
+
+use OpenOnMake\Check;
+use OpenOnMake\Paths;
+use OpenOnMake\CommandInfo;
+use OpenOnMake\Files\MigrationFile;
+
+class PathForCommandCommand extends AbstractPath
+{
+    public function handle(CommandInfo $commandInfo)
+    {
+        $commandClass = $commandInfo->getCommandClass();
+        $reflection = new \ReflectionClass($commandClass);
+        if (Check::isSubClassOfGeneratorCommand($reflection)) {
+            $pathMethod = new \ReflectionMethod($commandClass, 'getPath');
+            $pathMethod->setAccessible(true);
+
+            $qualifyMethod = new \ReflectionMethod($commandClass, 'qualifyClass');
+            $qualifyMethod->setAccessible(true);
+
+            $instance = $this->getCommandInstance($commandInfo);
+
+            $qualifiedName = $qualifyMethod->invokeArgs($instance, [$this->argName]);
+            return $pathMethod->invokeArgs($instance, [$qualifiedName]);
+        }
+        // Migration is a special cases that do not extend the GeneratorComand. We can handle them like this:
+        if ($reflection->getName() === Paths::getCommandPath('migration')) {
+            return MigrationFile::getLatestMigrationFile();
+        }
+    }
+
+    private function getCommandInstance($commandInfo)
+    {
+        $container = app();
+
+        $instance = $container->make(Paths::getCommandClass($commandInfo->getCommandString()));
+        $instance->setLaravel($container);
+
+        return $instance;
+    }
+}
