79f1d3df1955272b9d595a418a619cd18f06a9ad
Andrew huggins <andrewhuggins@gmail.com>
2020-08-29 17:42:25 -0400

    always return a path from FilePath->determine()

--- src/PathGetters/FilePath.php
+++ src/PathGetters/FilePath.php
@@ -5,24 +5,22 @@ namespace OpenOnMake\PathGetters;
 use OpenOnMake\File;
 use OpenOnMake\CommandInfo;
 use OpenOnMake\Openers\MigrationFile;
-use OpenOnMake\PathGetters\PathForCommandCommand;
 
 class FilePath
 {
     private $file;
+    private $migration;
     
-    public function __construct(File $file)
+    public function __construct(File $file, MigrationFile $migration)
     {
         $this->file = $file;
+        $this->migration = $migration;
     }
 
     public function determine(CommandInfo $commandInfo)
     {
         if ($commandInfo->isMigrationCommand()) {
-            MigrationFile::open();
-            exit;
-        } elseif ($commandInfo->isCommandCommand()) {
-            return (new PathForCommandCommand)->handle($commandInfo);
+            return $this->migration->getLatestMigration();
         }
         return $this->file->find($commandInfo);
     }
